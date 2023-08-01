df8ad56c2beed58e5b4d89db41bff960ee342bd7
SR.Asadollahi <Seyedreza.asadollahi@gmail.com>
2023-08-01 15:33:51 +0330

    1402/05/10 - improve linked-list and write some test

--- linked-list.js
+++ linked-list.js
@@ -11,8 +11,8 @@ class LinkedList {
     increaseIndex() { this.lenght++; }
     decreaseIndex() { this.lenght--; }
 
-    getByIndex(index) {
-        if (index >= this.lenght)
+    get(index) {
+        if (index >= this.lenght || index < 0)
             throw new Error('out of range')
 
         let node = this.head;
@@ -22,44 +22,68 @@ class LinkedList {
         return node;
     }
 
+    set(value, index) {
+        if (index >= this.lenght || index < 0)
+            throw new Error('out of range')
+
+        let node = this.get(index);
+        node.value = value;
+
+        return node;
+    }
+
     unshift(value) {
         this.head = new LinkedListNode(value, this.head)
         this.increaseIndex();
+        if (this.lenght === 1)
+            this.tail = this.head
     }
 
     shift() {
+        let deletedNode;
         if (this.lenght > 1) {
             let deletedNode = this.head;
             this.head = this.head.next;
             this.decreaseIndex();
             return deletedNode;
-        } else if (this.lenght) {
-            this.head = this.tail = nul
+        } else if (this.lenght === 1) {
+            this.head = this.tail = null
             this.decreaseIndex();
         } else
             throw new Error('list is empty')
     }
 
     insert(value, index) {
-        let node = this.getByIndex(index - 1); // to access previous item
+        if (index === this.lenght) return this.push(value)
+        else if (index === 0) return this.unshift(value)
+        let node = this.get(index - 1); // to access previous item
         node.next = new LinkedListNode(value, node.next);
-        if ((index + 1) === this.lenght)
-            this.tail = node.next;
         this.increaseIndex();
     }
 
     push(value) {
-        let node = this.getByIndex(this.lenght - 1);// to access last item
-        node.next = new LinkedListNode(value, null);
-        this.tail = node.next;
+        if (!this.head) {
+            this.head = this.tail = new LinkedListNode(value, null);
+        } else {
+            this.tail.next = new LinkedListNode(value, null);
+            this.tail = this.tail.next;
+        }
         this.increaseIndex();
     }
 
     pop() {
-        let node = this.getByIndex(this.lenght - 2);
-        let deletedNode = node.next;
-        node.next = null;
-        this.tail = node;
+        let deletedNode;
+        if (!this.lenght)
+            return deletedNode;
+        if (this.lenght === 1) {
+            deletedNode = this.head;
+            this.head = this.tail = null;
+        } else {
+            let node = this.get(this.lenght - 2);
+            deletedNode = node.next;
+            node.next = null;
+            this.tail = node;
+        }
         this.decreaseIndex();
         return deletedNode
     }
@@ -89,7 +113,27 @@ class LinkedList {
         }
     }
 
-    deleteByIndex(value) { }
+    deleteByIndex(index) {
+        let deletedNode;
+        if (this.lenght === 0 || this.lenght <= index) {
+            return null
+        } if (index === 0 && this.lenght === 1) {
+            deletedNode = this.head;
+            this.head = this.tail = null;
+        } else if (index === 0) {
+            deletedNode = this.head;
+            this.head = this.head.next;
+        } else {
+            let prevNode = this.get(index - 1);
+            deletedNode = prevNode.next;
+            prevNode.next = prevNode.next.next;
+
+            if (prevNode.next == null)// or index + 1 === this.lenght
+                this.tail = prevNode
+        }
+        this.decreaseIndex();
+        return deletedNode;
+    }
 }
 
 class LinkedListNode {
@@ -103,3 +147,30 @@ class LinkedListNode {
         // this.before = null;
     }
 }
+
+
+// test case
+(function() {
+    let list = new LinkedList(2);
+
+    list.unshift(1);
+    console.log(list);
+
+    list.push(3);
+    console.log(list);
+    
+    list.insert(1.5, 1);
+    console.log(list);
+
+    list.delete(1.5);
+    console.log(list);
+
+    console.log('pop', list.pop())
+    console.log(list)
+    
+    console.log('shift', list.shift())
+    console.log(list)
+    
+    console.log('deleteByIndex', list.deleteByIndex(0))
+    console.log(list)
+})()
